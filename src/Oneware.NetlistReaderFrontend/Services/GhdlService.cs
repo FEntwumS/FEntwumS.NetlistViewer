27353f24472359a9dc2ad47d4a88cd69ed53cf64
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2025-01-22 16:13:45 +0100

    Use VHDL Standard from project settings

--- src/Oneware.NetlistReaderFrontend/Services/GhdlService.cs
+++ src/Oneware.NetlistReaderFrontend/Services/GhdlService.cs
@@ -13,10 +13,10 @@ public class GhdlService : IGhdlService
     private ISettingsService _settingsService;
     private IChildProcessService _childProcessService;
     private IToolExecuterService _toolExecuterService;
-    
+
     private string _ghdlPath = string.Empty;
     private string _vhdlStandard = string.Empty;
-    
+
     public GhdlService()
     {
         _dockService = ServiceManager.GetService<IDockService>();
@@ -30,11 +30,13 @@ public class GhdlService : IGhdlService
         _settingsService.GetSettingObservable<string>("GhdlModule_GhdlPath").Subscribe(x => _ghdlPath = x);
         _settingsService.GetSettingObservable<string>("NetlistViewer_VHDL_Standard").Subscribe(x => _vhdlStandard = x);
     }
-    
+
     public async Task<bool> ElaborateDesignAsync(IProjectFile file)
     {
-        _dockService.Show<IOutputService>();
+        string vhdlStandard;
         
+        _dockService.Show<IOutputService>();
+
         string top = Path.GetFileNameWithoutExtension(file.FullPath);
 
         if (file.Root is not UniversalFpgaProjectRoot root)
@@ -45,7 +47,8 @@ public class GhdlService : IGhdlService
 
         if (_ghdlPath == string.Empty)
         {
-            _logger.Error("No GHDL path specified. Please install the GHDL-Plugin and specify the path to the executable, if necessary.");
+            _logger.Error(
+                "No GHDL path specified. Please install the GHDL-Plugin and specify the path to the executable, if necessary.");
         }
 
         if (!File.Exists(_ghdlPath))
@@ -53,24 +56,34 @@ public class GhdlService : IGhdlService
             _logger.Error("GHDL path does not point to ghdl executable");
         }
         
-        IEnumerable<string> vhdlFiles = root.Files
-            .Where(x => !root.CompileExcluded.Contains(x))  // Exclude excluded files
-            .Where(x => x.Extension is ".vhd" or ".vhdl")   // Include only VHDL files
-            .Where(x=> !root.TestBenches.Contains(x))       // Exclude testbenches
-            .Select(x => x.FullPath);
+        vhdlStandard = root.GetProjectProperty("VHDL_Standard");
+
+        if (vhdlStandard == null)
+        {
+            _logger.Error("GHDL standard is not set");
+            return false;
+        }
         
+        _logger.Log($"Found VHDL Standard {vhdlStandard}");
+
+        IEnumerable<string> vhdlFiles = root.Files
+            .Where(x => !root.CompileExcluded.Contains(x)) // Exclude excluded files
+            .Where(x => x.Extension is ".vhd" or ".vhdl") // Include only VHDL files
+            .Where(x => !root.TestBenches.Contains(x)) // Exclude testbenches
+            .Select(x => x.FullPath);
+
         string workingDirectory = Path.Combine(file.Root!.FullPath, "build", "netlist");
 
         if (Directory.Exists(workingDirectory))
         {
             Directory.Delete(workingDirectory, true);
         }
-        
+
         Directory.CreateDirectory(workingDirectory);
-            
+
         List<string> ghdlOptions = [];
-        
-        ghdlOptions.Add("--std=" + _vhdlStandard);
+
+        ghdlOptions.Add("--std=" + vhdlStandard);
         ghdlOptions.Add("-Pbuild");
 
         List<string> ghdlImportArgs = ["-i"];
@@ -81,52 +94,55 @@ public class GhdlService : IGhdlService
         string stdout = string.Empty;
         string stderr = string.Empty;
 
-        (success, stdout, stderr) = await _toolExecuterService.ExecuteToolAsync(_ghdlPath, ghdlImportArgs, workingDirectory);
+        (success, stdout, stderr) =
+            await _toolExecuterService.ExecuteToolAsync(_ghdlPath, ghdlImportArgs, workingDirectory);
 
         if (!success)
         {
             _logger.Error("GHDL import failed");
             return false;
         }
-        
+
         List<string> ghdlMakeArgs = ["-m"];
         ghdlMakeArgs.AddRange(ghdlOptions);
         ghdlMakeArgs.Add(top);
-        
-        (success, stdout, stderr) = await _toolExecuterService.ExecuteToolAsync(_ghdlPath, ghdlMakeArgs, workingDirectory);
+
+        (success, stdout, stderr) =
+            await _toolExecuterService.ExecuteToolAsync(_ghdlPath, ghdlMakeArgs, workingDirectory);
 
         if (!success)
         {
             _logger.Error("GHDL import failed");
             return false;
         }
-        
+
         return success;
     }
 
     public async Task<bool> CrossCompileDesignAsync(IProjectFile file)
     {
         string workingDirectory = Path.Combine(file.Root!.FullPath, "build", "netlist");
-        
+
         string top = Path.GetFileNameWithoutExtension(file.FullPath);
 
-        List<string> ghdlOptions = [ "--out=verilog" ];
+        List<string> ghdlOptions = ["--out=verilog"];
         // ghdlOptions.Add("-o=design.v");  // TODO rework
         // When a new version of GHDL is available, this parameter will allow us to write the result directly to a file,
         // instead of needing to use File.WriteAllTextAsync
 
-        List<string> ghdlSynthArgs = ["--synth", "--no-formal", "-Pbuild" ];
+        List<string> ghdlSynthArgs = ["--synth", "--no-formal", "-Pbuild"];
         ghdlSynthArgs.AddRange(ghdlOptions);
         ghdlSynthArgs.Add(top);
-        
+
         bool success = false;
         string stderr = string.Empty;
         string stdout = string.Empty;
-        
-        (success, stdout, stderr) = await _toolExecuterService.ExecuteToolAsync(_ghdlPath, ghdlSynthArgs, workingDirectory);
-        
+
+        (success, stdout, stderr) =
+            await _toolExecuterService.ExecuteToolAsync(_ghdlPath, ghdlSynthArgs, workingDirectory);
+
         await File.WriteAllTextAsync(Path.Combine(workingDirectory, "design.v"), stdout);
-        
+
         return success;
     }
 
