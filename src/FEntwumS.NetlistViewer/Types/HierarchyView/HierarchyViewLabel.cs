7677aa170a2d03715cb1a9eb2f95df01c703d5fe
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2025-06-20 14:57:25 +0200

    Add all types for hierarchy view

---
+++ src/FEntwumS.NetlistViewer/Types/HierarchyView/HierarchyViewLabel.cs
@@ -0,0 +1,12 @@
+﻿namespace FEntwumS.NetlistViewer.Types.HierarchyView;
+
+public class HierarchyViewLabel : HierarchyViewShape
+{
+    private string? _content;
+
+    public string Content
+    {
+        get => this._content ?? "";
+        set => this._content = value;
+    }
+}
