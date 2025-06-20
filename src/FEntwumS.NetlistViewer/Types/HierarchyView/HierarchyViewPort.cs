5eff41bde99989a1ce5e44055fdb6c7663c13101
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2025-06-20 14:57:25 +0200

    Add all types for hierarchy view

---
+++ src/FEntwumS.NetlistViewer/Types/HierarchyView/HierarchyViewPort.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Media;
+
+namespace FEntwumS.NetlistViewer.Types.HierarchyView;
+
+public class HierarchyViewPort : HierarchyViewShape
+{
+    // arrow_swap_regular
+    // arrow_export_rtl_regular
+    // arrow_left_regular
+    // arrow_right_regular
+    // arrow_import_regular
+    
+    private StreamGeometry? _geometry;
+
+    public StreamGeometry Geometry
+    {
+        get => _geometry ?? new StreamGeometry();
+        set => _geometry = value;
+    }
+}
---
+++ src/FEntwumS.NetlistViewer/Types/HierarchyView/HierarchyViewShape.cs
@@ -0,0 +1,19 @@
+﻿namespace FEntwumS.NetlistViewer.Types.HierarchyView;
+
+public class HierarchyViewShape : HierarchyViewElement
+{
+    private double _width;
+    private double _height;
+
+    public double Width
+    {
+        get => this._width;
+        set => this._width = value;
+    }
+
+    public double Height
+    {
+        get => this._height;
+        set => this._height = value;
+    }
+}
