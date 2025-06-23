0b49a4b3a2e2e812588c11395fde4efad36ea3ea
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2025-06-23 16:32:20 +0200

    Add fontsize parameter in label

--- src/FEntwumS.NetlistViewer/Types/HierarchyView/HierarchyViewLabel.cs
+++ src/FEntwumS.NetlistViewer/Types/HierarchyView/HierarchyViewLabel.cs
@@ -3,10 +3,17 @@
 public class HierarchyViewLabel : HierarchyViewShape
 {
     private string? _content;
+    private double _fontSize;
 
     public string Content
     {
         get => this._content ?? "";
         set => this._content = value;
+    }
+
+    public double FontSize
+    {
+        get => this._fontSize;
+        set => this._fontSize = value;
     }
 }
