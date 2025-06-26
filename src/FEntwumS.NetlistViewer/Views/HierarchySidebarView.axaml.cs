27efe5fcb4dcac07a4b6ec37769b9a41c81a9e5e
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2025-06-26 17:02:24 +0200

    Start work on completing sidebar view of hierarchy

---
+++ src/FEntwumS.NetlistViewer/Views/HierarchySidebarView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace FEntwumS.NetlistViewer.Views;
+
+public partial class HierarchySidebarView : UserControl
+{
+    public HierarchySidebarView()
+    {
+        InitializeComponent();
+    }
+}
