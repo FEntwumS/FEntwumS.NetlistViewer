53fabb58d3d1e1d962dc2351f94a36a9f8fe74f9
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2025-08-07 15:32:35 +0200

    Remove automatic backend startup

--- src/FEntwumS.NetlistViewer/FEntwumSNetlistReaderFrontendModule.cs
+++ src/FEntwumS.NetlistViewer/FEntwumSNetlistReaderFrontendModule.cs
@@ -434,9 +434,6 @@ public class FEntwumSNetlistReaderFrontendModule : IModule
 
         logger.Log("FEntwumS.NetlistViewer: Subscribed relevant services to the settings relevant to them");
 
-        logger.Log("Starting the backend");
-        _ = ServiceManager.GetService<FrontendService>().StartBackendIfNotStartedAsync();
-
         ServiceManager.GetService<IApplicationStateService>().RegisterShutdownAction(() =>
         {
             try
