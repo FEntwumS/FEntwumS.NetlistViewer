fd083d4a91a2e31ec669d86612ef4536f7f95a6e
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2025-08-14 16:23:15 +0200

    Use ContainerLocator instead of manually saved IContainerProvider instance to resolve services

--- src/FEntwumS.NetlistViewer/Services/ServiceManager.cs
+++ src/FEntwumS.NetlistViewer/Services/ServiceManager.cs
@@ -5,45 +5,38 @@ namespace FEntwumS.NetlistViewer.Services;
 
 public class ServiceManager
 {
-	private static IContainerProvider? _containerProvider;
-
-	public ServiceManager(IContainerProvider? containerProvider)
-	{
-		_containerProvider = containerProvider;
-	}
-
 	public static IJsonLoader GetJsonLoader()
 	{
-		return _containerProvider.Resolve<IJsonLoader>();
+		return ContainerLocator.Current.Resolve<IJsonLoader>();
 	}
 
 	public static IViewportDimensionService? GetViewportDimensionService()
 	{
-		return _containerProvider.Resolve<IViewportDimensionService>();
+		return ContainerLocator.Current.Resolve<IViewportDimensionService>();
 	}
 
 	public static ILogger GetLogger()
 	{
-		return _containerProvider.Resolve<ILogger>();
+		return ContainerLocator.Current.Resolve<ILogger>();
 	}
 
 	public static ICustomLogger GetCustomLogger()
 	{
-		return _containerProvider.Resolve<ICustomLogger>();
+		return ContainerLocator.Current.Resolve<ICustomLogger>();
 	}
 
 	public static ILanguageManager GetLanguageManager()
 	{
-		return _containerProvider.Resolve<ILanguageManager>();
+		return ContainerLocator.Current.Resolve<ILanguageManager>();
 	}
 
 	public static IHashService GetHashService()
 	{
-		return _containerProvider.Resolve<IHashService>();
+		return ContainerLocator.Current.Resolve<IHashService>();
 	}
 
 	public static T GetService<T>()
 	{
-		return _containerProvider.Resolve<T>();
+		return ContainerLocator.Current.Resolve<T>();
 	}
 }
