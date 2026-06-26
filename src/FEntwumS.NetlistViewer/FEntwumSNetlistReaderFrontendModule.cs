70fd10882a35cf40f7836509a72b20438c4a732c
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2026-06-26 18:45:52 +0200

    Add new backend version

--- src/FEntwumS.NetlistViewer/FEntwumSNetlistReaderFrontendModule.cs
+++ src/FEntwumS.NetlistViewer/FEntwumSNetlistReaderFrontendModule.cs
@@ -264,6 +264,27 @@ public class FEntwumSNetlistReaderFrontendModule : OneWareModuleBase
 						]
 					}
 				]
+			},
+			new PackageVersion()
+			{
+				Version = "0.12.2",
+				Targets =
+				[
+					new PackageTarget()
+					{
+						Target = "all",
+						Url =
+							"https://github.com/FEntwumS/NetlistReaderBackend/releases/download/v0.12.2/fentwums-netlist-reader-server-v0.12.2.tar.gz",
+						AutoSetting =
+						[
+							new PackageAutoSetting()
+							{
+								RelativePath = "fentwums-netlist-reader",
+								SettingKey = FentwumSNetlistViewerSettingsHelper.NetlistPathSettingKey,
+							}
+						]
+					}
+				]
 			}
 		]
 	};
