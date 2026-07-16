c0a3a4eed0b844298a1c982e23b42cbe800fb0f5
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2026-07-16 15:22:14 +0200

    Add Child element to PanningControl

--- src/FEntwumS.Common/Controls/PanningControl.cs
+++ src/FEntwumS.Common/Controls/PanningControl.cs
@@ -5,23 +5,29 @@ using Avalonia.Media;
 
 namespace FEntwumS.Common.Controls;
 
-public class PanningControl<T> : Control where T : PositionableSubControl
+public class PanningControl : Control
 {
     #region Properties
     
     private double OffsetX { get; set; }
     
     public static readonly StyledProperty<double> OffsetXProperty =
-	    AvaloniaProperty.Register<PanningControl<T>, double>(nameof(OffsetX),
+	    AvaloniaProperty.Register<PanningControl, double>(nameof(OffsetX),
 		    defaultBindingMode: BindingMode.TwoWay,
 		    defaultValue: 0.0d);
     
     private double OffsetY { get; set; }
     
     public static readonly StyledProperty<double> OffsetYProperty =
-	    AvaloniaProperty.Register<PanningControl<T>, double>(nameof(OffsetY),
+	    AvaloniaProperty.Register<PanningControl, double>(nameof(OffsetY),
 		    defaultBindingMode: BindingMode.TwoWay,
 		    defaultValue: 0.0d);
+    
+    private PositionableSubControl? Item { get; set; }
+
+    public static readonly StyledProperty<PositionableSubControl> ChildProperty =
+	    AvaloniaProperty.Register<PanningControl, PositionableSubControl>(nameof(Item),
+		    defaultBindingMode: BindingMode.TwoWay);
     
     #endregion
     
