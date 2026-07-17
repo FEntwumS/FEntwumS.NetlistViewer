07ddb735c6e0afa9fcbb0ed8622ce98239e5cfe5
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2026-07-17 17:29:17 +0200

    Expand theme

--- src/FEntwumS.Common/Types/NetlistTheme.cs
+++ src/FEntwumS.Common/Types/NetlistTheme.cs
@@ -5,6 +5,9 @@ namespace FEntwumS.Common.Types;
 public struct NetlistTheme
 {
     public Typeface Typeface { get; set; }
+    
+    #region Brushes
+    
     public Brush BackGroundBrush { get; set; }
     public Brush BorderBrush { get; set; }
     public Brush DropShadowBrush { get; set; }
@@ -12,5 +15,27 @@ public struct NetlistTheme
     public Brush FillBrush { get; set; }
     public Brush TextBrush { get; set; }
     public Brush HighlightBrush { get; set; }
+    
+    #endregion
+
+    #region Pens
+
+    public Pen BorderPen { get; set; }
+    public Pen DropShadowPen { get; set; }
+    public Pen EdgePen { get; set; }
+    public Pen BundledEdgePen { get; set; }
+    public Pen NotConnectedPen { get; set; }
+    public Pen HighLightPen { get; set; }
+
+    #endregion
+    
+    #region Colors
+    
+    #endregion
+
+    public void RegenerateBrushesAndPens(double scale = 1.0)
+    {
+        
+    }
     
 }
