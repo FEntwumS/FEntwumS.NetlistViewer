e754845c99de980139c44bb96f67364b5898c38b
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2026-07-17 17:23:27 +0200

    Add more brushes to the netlist theme

--- src/FEntwumS.Common/Types/NetlistTheme.cs
+++ src/FEntwumS.Common/Types/NetlistTheme.cs
@@ -5,4 +5,12 @@ namespace FEntwumS.Common.Types;
 public struct NetlistTheme
 {
     public Typeface Typeface { get; set; }
+    public Brush BackGroundBrush { get; set; }
+    public Brush BorderBrush { get; set; }
+    public Brush DropShadowBrush { get; set; }
+    public Brush EdgeBrush { get; set; }
+    public Brush FillBrush { get; set; }
+    public Brush TextBrush { get; set; }
+    public Brush HighlightBrush { get; set; }
+    
 }
