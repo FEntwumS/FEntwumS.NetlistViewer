0e6664291bf04151948146d9c38e30873863e3d3
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2026-07-22 15:59:24 +0200

    Add rendering for edges

--- src/FEntwumS.Common/Controls/GraphEdgeControl.cs
+++ src/FEntwumS.Common/Controls/GraphEdgeControl.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Collections;
+using Avalonia.Data;
 using Avalonia.Media;
 using Avalonia.Rendering;
 
@@ -19,18 +20,43 @@ public class GraphEdgeControl : GenericGraphElementControl, ICustomHitTest
 			o => o._points,
 			(o, v) => o._points = v);
 
+	private bool _isThick
+	{
+		get => GetValue(IsThickProperty);
+		set => SetValue(IsThickProperty, value);
+	}
+	
+	public static readonly StyledProperty<bool> IsThickProperty =
+		AvaloniaProperty.Register<GraphEdgeControl, bool>(nameof(_isThick),
+			defaultBindingMode: BindingMode.TwoWay,
+			defaultValue: false);
+
 	#endregion
 
 	#region Variables
 
-	
+	private Geometry _contentGeometry = new PolylineGeometry([new Point(0, 0), new Point(0, 10)], false);
 
 	#endregion
 
 	#region Event handling
-	
 
-	
+	protected override void OnInitialized()
+	{
+		_contentGeometry = new PolylineGeometry(_points, false);
+		
+		base.OnInitialized();
+	}
+
+	protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs e)
+	{
+		if (e.Property == NetlistThemeProperty)
+		{
+			RegenerateDrawnElements();
+		}
+		
+		base.OnPropertyChanged(e);
+	}
 
 	#endregion
 
@@ -38,7 +64,15 @@ public class GraphEdgeControl : GenericGraphElementControl, ICustomHitTest
 
 	public override void Render(DrawingContext context)
 	{
+		Pen linePen = _isThick ? NetlistTheme.BundledEdgePen : NetlistTheme.EdgePen;
+		context.DrawGeometry(null, linePen, _contentGeometry);
+		
 		base.Render(context);
+	}
+
+	private void RegenerateDrawnElements()
+	{
+		_contentGeometry.Transform = new ScaleTransform(Scale, Scale);
 	}
 
 	#endregion
