c9b5680132a0adda5261e7eaf466175ebeeb42e7
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2026-07-23 16:54:03 +0200

    Add basic view & viewmodel for new netlist control

---
+++ src/FEntwumS.Common/ViewModels/NetlistViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Media;
+using OneWare.Essentials.ViewModels;
+
+namespace FEntwumS.Common.ViewModels;
+
+public class NetlistViewModel : ExtendedTool
+{
+	public NetlistViewModel() : base("NetlistViewer_Netlist")
+	{
+		
+	}
+}
---
+++ src/FEntwumS.Common/Views/NetlistView.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace FEntwumS.Common.Views;
+
+public partial class NetlistView : UserControl
+{
+	// For compiled bindings in code see: https://docs.avaloniaui.net/docs/fundamentals/coded-ui#compiled-bindings
+	// For thread-safe accesses to UI objects via the dispatcher, see https://docs.avaloniaui.net/docs/app-development/threading#avaloniaobjectdispatcher
+	private void ZoomToFitButton_OnClick(object? sender, RoutedEventArgs e)
+	{
+		PanningControl.ZoomToFit();
+	}
+}
